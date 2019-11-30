4bee3c4cd69559544dbefbd86980a7c6b745e70e
Paul Walker <paul.wwwalker2002@gmail.com>
2019-11-30 11:27:22 +0100

    added example template

---
+++ lib/LightData/examples/example.ino
@@ -0,0 +1,11 @@
+#include <LightData.h>
+
+LD::LightData ld(10/*Recieve Pin*/,11/*transmit Pin*/, 1250/*transmit speed*/);
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
