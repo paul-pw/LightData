fb3058e8f5d2c4d58e5796e776e44561b22e95e6
Paul Walker <paul.wwwalker2002@gmail.com>
2019-11-30 11:30:00 +0100

    small changes in main for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <Arduino.h>
 #include <LightData.h>
 
-LD::LightData ld(10/*Recieve Pin*/,11/*transmit Pin*/, 9600/*check rate*/, 1250/*transmit speed*/);
+LD::LightData ld(10/*Recieve Pin*/,11/*transmit Pin*/, 1250/*transmit speed*/);
 
 void setup() {
   // put your setup code here, to run once:
-  ld.setMethod(LD::LightData::Flags::SuperSpeed);
 }
 
 void loop() {
+  int arr [] = { 1,2,3,4,2,34,2334,2,3,5 };
+  ld.send(arr, ARRAY_SIZE(arr));
+  ld.send('H');
   // put your main code here, to run repeatedly:
+
+  //use bitset and bitread to set and read bits
 }
